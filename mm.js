b61c7fe6e24e7cadd0fc4d5896808cc93a4d2e66
sonamng <86510537+sonamng@users.noreply.github.com>
2021-10-05 19:45:08 +0530

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,43 @@
+// console.log("welcome")
+
+// var myResult= 2+3;
+
+// console.log(myResult);
+
+// var Name = "komal";
+// var name = "swati";
+// console.log(Name+name)
+
+
+
+// var string1= "I am staying in the \"Sarjapur Campus\""
+// console.log(string1)
+
+
+// var name = "navgurukul";
+// var num = 29;
+// var result = name + num;
+// console.log(result);
+
+
+// var measure = "height";
+// var val = 1
+// var result = measure + val;
+// console.log(result);
+
+
+// var num=24
+// var a="kumar"
+// console.log("kumar")
+// console.log(num)
+
+
+var num1;
+num1 = 6;
+console.log(num1)
+
+const name="sonam";
+console.log(typeof (name))
+
+let num=1.2
+console.log(typeof num)
---
+++ question.py
@@ -0,0 +1,57 @@
+
+# variable_x=123
+
+# a=9999
+# print (variable_x, a)
+
+
+
+# a='hello world'
+
+# b='navgurukul'
+
+# print(a,b)
+
+# a = 3
+# b = 4
+# c = a * b
+# a = b * c
+# b = a + c
+# c = a + b + c
+# print(c)
+
+
+# var_12=2.2
+# as_var="sonam"
+# as_12=12
+# print(var_12,as_var,as_12)
+
+
+# num=6.8
+# num1=5
+# print(type(num+num1))
+# print(num+num1)
+
+
+# x = 20
+# y = 5
+# result = x + True + y - False
+# print(result)
+
+# numa=int(input("enter the number"))
+# numb=int(input("enter the number"))
+# symbols=input("enter the symbols")
+# if symbols=="+":
+#      print(numa+numb)
+
+
+# a="1.2"
+# b="2.2"
+# print(a+b)
+
+# print(22+(2//9)**6*3%4-8/4)
+
+
+
+
+
